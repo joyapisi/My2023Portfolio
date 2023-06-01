340fea3318871c198e08ae597da767d8598297af
Joy Phoebe <joyapisi@gmail.com>
2023-06-01 18:50:20 +0300

    fx landng page

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import bwmap from './backgrounds/bw-map.jpeg';
+import bwmap from './backgrounds/bw-map.jpg';
 import nairobi from './backgrounds/nairobi.png';
 import whiteabstract from './backgrounds/white-abstract.png';
 import worldmap from './backgrounds/world-map.png';
@@ -6,7 +6,7 @@ import worldmap from './backgrounds/world-map.png';
 import logo from './logo/logo-black.png';
 import logotext from './logo/logo-text-black.png';
 
-import shaq from './personal/shaq.png';
+// import shaq from './personal/shaq.png';
 
 import backend from './icons/backend.png';
 import ux from './icons/ux.png';
@@ -57,7 +57,7 @@ export {
   worldmap,
   logo,
   logotext,
-  shaq,
+  // shaq,
   backend,
   ux,
   frontend,
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { styles } from '../styles';
 import { navLinks } from '../constants';
-import { shaq, bwmap, worldmap } from '../assets';
+import { bwmap, worldmap } from '../assets';
 
 const Hero = () => {
   return (
@@ -37,17 +37,17 @@ const Hero = () => {
           <div>
             <h1
               className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
-              Hi, I'm{' '}
+              Hi!{' '}
               <span
                 className="sm:text-battleGray sm:text-[90px] 
-                text-eerieBlack text-[50px] font-mova
+                text-eerieBlack text-[30px] font-mova
                 font-extrabold uppercase">
-                Shaquille
+                 I&apos;m Joy
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
-              Lorem ipsum dolor sit amet. <br className="sm:block hidden" />
-              consectetur adipisicing elit deleniti, voluptas.
+              A Front-End. <br className="sm:block hidden" />
+              develpoer.
             </p>
           </div>
           <div
@@ -82,12 +82,12 @@ const Hero = () => {
 
         {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
         <div>
-          <img
+          <div
             className="absolute bottom-0 ml-[50vw] 
             lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
             sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
-            src={shaq}
-            alt="shaquille"
+            // src={shaq}
+            // alt="joy"
           />
         </div>
       </section>
