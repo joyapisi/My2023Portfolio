bac5eb3e17010d904d5671fe8167c663c2d7d77e
Joy Phoebe <joyapisi@gmail.com>
2023-09-06 02:34:13 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -86,7 +86,7 @@ const Hero = () => {
             className="absolute bottom-0 ml-[50vw] 
             lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
             sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
-            // src={shaq}
+            // src={joy}
             // alt="joy"
           />
         </div>
