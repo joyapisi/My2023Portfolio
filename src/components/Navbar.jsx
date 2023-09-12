1c2ddaef3e027731ce3d945d1f65993d4777206a
Joy Phoebe <joyapisi@gmail.com>
2023-09-12 12:18:35 +0300

    Remove commented code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,13 +21,10 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}>
           <img
-            src={logo} // your logo comes here
+            src={logo}
             alt="logo"
             className="sm:w-[50px] sm:h-[50px] w-[45px] h-[45px] object-contain"
           />
-
-          {/* if you have text you want besides your logo it comes here.
-          Otherwise delete this if you don't need it. */}
           <img
             src={logotext}
             alt="logo"
