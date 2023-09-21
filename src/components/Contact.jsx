a1af04e1a16486e8607d4bf4002ec3ceda95aefa
Joy Phoebe <joyapisi@gmail.com>
2023-09-21 15:48:58 +0300

    Restyle in css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -118,13 +118,7 @@ const Contact = () => {
 
           <button
             type="submit"
-            className="live-demo flex justify-center sm:gap-4 
-            gap-3 sm:text-[20px] text-[16px] text-timberWolf 
-            font-bold font-beckman items-center py-5
-            whitespace-nowrap sm:w-[130px] sm:h-[50px] 
-            w-[100px] h-[45px] rounded-[10px] bg-night 
-            hover:bg-battleGray hover:text-eerieBlack 
-            transition duration-[0.2s] ease-in-out"
+            className="live-demo flex submit-contact"
             onMouseOver={() => {
               document
                 .querySelector('.contact-btn')
